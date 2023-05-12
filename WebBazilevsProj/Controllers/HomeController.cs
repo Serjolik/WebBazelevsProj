0fe49575b30004db88e87178f9b2df2a6330bf02
Sergey <Seregsa2014@gmail.com>
2023-05-12 23:02:52 +0300

    Update
    Added page with send info to google search

--- WebBazilevsProj/Controllers/HomeController.cs
+++ WebBazilevsProj/Controllers/HomeController.cs
@@ -28,5 +28,6 @@ namespace WebBazilevsProj.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
     }
 }
---
+++ WebBazilevsProj/Controllers/ValuesController2.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Text;
+using WebBazilevsProj.Models;  
+
+namespace WebBazilevsProj.Controllers
+{
+    [Route("Search")]
+    public class GoogleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("Browse")]
+        public async Task<IActionResult> Search(string query)
+        {
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync($"https://www.google.com/search?q={query}");
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+            ViewBag.ResultText = responseBody;
+
+            return View("Index");
+        }
+    }
+}
