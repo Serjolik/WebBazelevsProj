49c9f8921494fee0b6f86df2a928c10dc925b29f
Sergey <Seregsa2014@gmail.com>
2023-05-14 21:18:33 +0300

    Update
    Test variant

--- WebBazilevsProj/Controllers/ValuesController1.cs
+++ WebBazilevsProj/Controllers/ValuesController1.cs
@@ -1,16 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
+﻿using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
+//"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"
 namespace WebBazilevsProj.Controllers
 {
-    [Route("ChatOpenAI")]
-    public class ChatOpenAIController : Controller
+    [Route("/Home/Chat")]
+    public class ChatController : Controller
     {
         private readonly HttpClient _httpClient;
 
-        public ChatOpenAIController(HttpClient httpClient)
+        public ChatController(HttpClient httpClient)
         {
             _httpClient = httpClient;
         }
@@ -24,37 +27,13 @@ namespace WebBazilevsProj.Controllers
         [HttpPost]
         public async Task<IActionResult> Index(string message)
         {
-            // Replace YOUR_API_KEY with your actual OpenAI API key
-            var apiKey = "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";
+            var requestData = new { message };
+            var requestBody = JsonSerializer.Serialize(requestData);
+            var content = new StringContent(requestBody, Encoding.UTF8, "application/json");
+            var response = await _httpClient.PostAsync("https://api.openai.com/v1/engines/davinci-codex/completions", content);
+            var responseBody = await response.Content.ReadAsStringAsync();
 
-            // Create the request data
-            var requestData = new { prompt = message, temperature = 0.5, max_tokens = 50 };
-
-            // Convert the request data to a JSON string
-            var requestDataJson = JsonConvert.SerializeObject(requestData);
-
-            // Create the request content
-            var requestContent = new StringContent(requestDataJson, Encoding.UTF8, "application/json");
-
-            // Set the authorization header
-            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Authorization", apiKey);
-
-            // Make the request to the OpenAI API
-            var response = await _httpClient.PostAsync("https://api.openai.com/v1/engines/davinci-codex/completions", requestContent);
-
-            // Get the response content as a string
-            var responseContent = await response.Content.ReadAsStringAsync();
-
-            // Deserialize the response content to a dynamic object
-            dynamic responseObject = JsonConvert.DeserializeObject(responseContent);
-
-            // Get the text result from the response object
-            var result = responseObject.choices[0].text;
-
-            // Pass the result to the view
-            ViewBag.Result = result;
-
-            return View();
+            return Content(responseBody, "application/json");
         }
     }
 }
---
+++ WebBazilevsProj/Views/Chat/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebBazilevsProj.Views.Chat
+{
+    public class Index1Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
