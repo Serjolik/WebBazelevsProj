b8c74dba78e3bee2df5b127832236560a451117e
Sergey <Seregsa2014@gmail.com>
2023-05-15 11:01:12 +0300

    Update
    Chat gpt integration rewrite.
    redundant data removed.
    site header changed.

--- WebBazilevsProj/Controllers/HomeController.cs
+++ WebBazilevsProj/Controllers/HomeController.cs
@@ -1,34 +1,52 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using OpenAI_API.Completions;
+using OpenAI_API;
 using System.Diagnostics;
+using System.Net.Http.Headers;
+using System.Text;
 using WebBazilevsProj.Models;   
 
 namespace WebBazilevsProj.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+        private readonly HttpClient httpClient = new HttpClient();
 
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+        [Route("Home/Manual")]
+        public IActionResult Manual()
         {
             return View();
         }
 
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        [HttpPost]
+        public IActionResult Index(string searchText)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            //your OpenAI API key
+            string apiKey = "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";
+            string answer = string.Empty;
+            var openai = new OpenAIAPI(apiKey);
+            CompletionRequest completion = new CompletionRequest();
+            completion.Prompt = searchText;
+            completion.Model = OpenAI_API.Models.Model.DavinciText;
+            completion.MaxTokens = 4000;
+            var result = openai.Completions.CreateCompletionAsync(completion);
+            if (result != null)
+            {
+                foreach (var item in result.Result.Completions)
+                {
+                    answer = item.Text;
+                }
+            }
+            ViewBag.Answer = answer;
+            ViewBag.Text = searchText;
+            return View();
         }
-
     }
 }
